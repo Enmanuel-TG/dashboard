3377b4cd24004e8b405818b0dfcdc5d6d7cdacc1
Enmanuel-TG <enmanueltg7@gmail.com>
2025-03-13 16:47:26 -0400

    add error Page

---
+++ src/app/dashboard/pokemons/components/PokemonsGrid.tsx
@@ -0,0 +1,19 @@
+import { SimplePokemon } from "@/app/pokemons";
+import { PokemonCard } from "./PokemonCard";
+
+interface PokemonsCardProps {
+  pokemons: SimplePokemon[];
+}
+
+export const PokemonsGrid = ({ pokemons }: PokemonsCardProps) => {
+  return (
+    <div className=" flex flex-col ">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        {pokemons.map((pokemons)=> (
+          <PokemonCard key={pokemons.id} pokemon={pokemons} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,33 +1,38 @@
 import { pokemonResponse, SimplePokemon } from "@/app/pokemons";
-import Image from "next/image";
-export const getPokemons = async (limit: number, offset: number): Promise<SimplePokemon[]> => {
-    const response: pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
-        .then((res) => res.json());
+import { PokemonsGrid } from "./components/PokemonsGrid";
 
-    const pokemons = response.results.map(pokemon => ({
-        id: parseInt(pokemon.url.split('/')[6]),
-        name: pokemon.name,
-    }))
+export const getPokemons = async (
+  limit: number,
+  offset: number
+): Promise<SimplePokemon[]> => {
+  const response: pokemonResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
 
-    return pokemons;
-
-}
+  const pokemons = response.results.map((pokemon) => ({
+    id: parseInt(pokemon.url.split("/")[6]),
+    name: pokemon.name,
+  }));
+  return pokemons;
+};
 
 const Page = async () => {
-    const pokemons = await getPokemons(20, 0);
+  const pokemons = await getPokemons(121, 0);
+  //throw new Error("This is an error");
   return (
-      <div className=" flex flex-col ">
-          page
-          <div className="flex flex-wrap gap-10 items-center justify-center">
-              {pokemons.map(pokemon => (
-                  <div key={pokemon.id}>{pokemon.name}
-                      <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} width={100} height={100} alt={pokemon.name} />
-                  </div>
-              ))}
+    <div className=" flex flex-col ">
+      <span className="text-5xl my-2">
+        Pokemons list <small>Static</small>
+      </span>
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        {pokemons.map((pokemon) => (
+          <div key={pokemon.id}>
+            <PokemonsGrid pokemons={[pokemon]} />
           </div>
+        ))}
       </div>
-  )
-}
-
+    </div>
+  );
+};
 
 export default Page;
